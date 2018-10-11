7203ae287e007e9adb0915b48aca099fde822295
prabhugopal <gprabhu27@gmail.com>
2018-10-11 00:26:52 -0400

    Commit with error!..

--- src/main/java/c4f/aws/LamdbaMethodHandler.java
+++ src/main/java/c4f/aws/LamdbaMethodHandler.java
@@ -5,6 +5,6 @@ import com.amazonaws.services.lambda.runtime.Context;
 public class LamdbaMethodHandler {
     public String handleRequest(String input, Context ctx){
         ctx.getLogger().log("Input: " + input);
-        //return "[LamdbaMethodHandler] Hello, world!.. " + input;
+        //return "[LamdbaMethodHandler] Hello, world!.. " + input; 
     }
 }
